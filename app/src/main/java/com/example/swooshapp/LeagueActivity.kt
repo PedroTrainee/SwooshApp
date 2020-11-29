6745b08ae6328c98fe86d50cb091712621580e15
admin <pedro.caridade@innowave.tech>
2020-11-29 19:10:33 +0000

    lesson 7

--- app/src/main/java/com/example/swooshapp/LeagueActivity.kt
+++ app/src/main/java/com/example/swooshapp/LeagueActivity.kt
@@ -1,19 +1,59 @@
 package com.example.swooshapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
+        /*private lateinit var womensLeagueBtn: ToggleButton
+        private lateinit var coedLeagueBtn: ToggleButton
+        private lateinit var mensLeagueBtn: ToggleButton*/
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        /*womensLeagueBtn = findViewById(R.id.womensLeagueBtn)
+        coedLeagueBtn =  findViewById(R.id.coedLeagueBtn)
+        mensLeagueBtn =  findViewById(R.id.mensLeagueBtn)*/
+    }
+
+    fun onMensClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = "co-ed"
     }
 
     fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
+
+
 }
