bb20996ff29184a6f010d50b56b4d3015fc4a4ea
admin <pedro.caridade@innowave.tech>
2020-11-29 20:47:40 +0000

    lesson 9

--- app/src/main/java/com/example/swooshapp/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swooshapp/Controller/LeagueActivity.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.swooshapp.Model.Player
 import com.example.swooshapp.R
-import com.example.swooshapp.Utilities.EXTRA_LEAGUE
+/*import com.example.swooshapp.Utilities.EXTRA_LEAGUE*/
+import com.example.swooshapp.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+
+    var player = Player("", "")
         /*private lateinit var womensLeagueBtn: ToggleButton
         private lateinit var coedLeagueBtn: ToggleButton
         private lateinit var mensLeagueBtn: ToggleButton*/
@@ -28,27 +31,27 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked(view: View) {
         mensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "womens"
+        player.league = "womens"
     }
 
     fun onCoedClicked(view: View) {
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
     }
 
     fun leagueNextClicked(view: View) {
-        if (selectedLeague != "") {
+        if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         } else {
             Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
